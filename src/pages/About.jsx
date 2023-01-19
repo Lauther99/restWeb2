5409e2ee924fe9885a4fea917d80da984bab8ca0
Lauther Valladares <lautherharold27@gmail.com>
2023-01-19 15:02:52 -0500

    fixed some bugs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,13 +13,13 @@ const About = () => {
                 bgImage={aboutBg}
             />
             <article className='about-description' >
-                <h1 data-aos="slide-right">Somos</h1>
+                <h1 data-aos="fade">Somos</h1>
                 <HighlightedText
                     text={'Donde el Che'}
                     bgColor={'--main-bg-color'}
                     colorText={'--main-color'}
                 />
-                <p data-aos="slide-left">
+                <p data-aos="fade">
                     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose
                 </p>
             </article>
